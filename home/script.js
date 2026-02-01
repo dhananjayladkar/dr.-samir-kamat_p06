cb61f58b8b07c8d2b1a9c5a095e12a81f2c5049b
(Dj) Dhananjay <158987168+dhananjayladkar@users.noreply.github.com>
2026-02-01 17:09:41 +0530

    Delete home directory

--- home/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.qa .q').forEach(q=>{
-  q.addEventListener('click',()=>{
-    const a=q.nextElementSibling;
-    a.style.display = a.style.display==='block'?'none':'block';
-  });
-});
